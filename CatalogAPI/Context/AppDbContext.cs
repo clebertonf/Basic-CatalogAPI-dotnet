fbe464231c2f1d15ceedd7d7e2fedced503eb2b6
Cleberton Francisco <clebertonfgc@gmail.com>
2024-06-30 21:46:17 -0300

    Add EF and db Context

---
+++ CatalogAPI/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CatalogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogAPI.Context;
+public class AppDbContext : DbContext
+{
+    DbSet<Category> categories {  get; set; }
+    DbSet<Product> products { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
