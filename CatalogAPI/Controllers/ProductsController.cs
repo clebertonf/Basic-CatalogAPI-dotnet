181326b2da305e0af6e341274abe19631c2da1ca
Cleberton Francisco <clebertonfgc@gmail.com>
2024-07-02 23:01:31 -0300

    put, post and delete

--- CatalogAPI/Controllers/ProductsController.cs
+++ CatalogAPI/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using CatalogAPI.Context;
 using CatalogAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatalogAPI.Controllers
 {
@@ -25,12 +26,57 @@ namespace CatalogAPI.Controllers
         }
 
         [HttpGet("{id:int}")]
-        public ActionResult<Product> Get(int id)
+        public ActionResult<Product> GetById(int id)
         {
             var product = _appDbContext.products.FirstOrDefault(p => p.ProductId.Equals(id));
             if (product is null) return NotFound("Product not found!");
 
             return product;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Product product)
+        {
+            if (product is null) return BadRequest();
+
+            _appDbContext.products.Add(product);
+            _appDbContext.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = product.ProductId}, product);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, [FromBody] Product product)
+        {
+            if (!id.Equals(product.ProductId)) return BadRequest();
+
+            /*
+             * Pode ser feito assim, com algumas desvantagens:
+             * _appDbContext.Entry(product).State = EntityState.Modified;
+             _appDbContext.SaveChanges();
+            */
+
+            var productData = _appDbContext.products.FirstOrDefault(p => p.ProductId.Equals(id));
+            if (productData is null) return NotFound();
+
+            productData.Name = product.Name;
+            productData.Description = product.Description;
+
+            _appDbContext.SaveChanges();
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+           var product = _appDbContext.products.FirstOrDefault(p => p.ProductId.Equals(id));
+           if (product is null) return NotFound();
+
+           _appDbContext.products.Remove(product);
+           _appDbContext.SaveChanges();
+
+            return NoContent();
         }
     }
 }
