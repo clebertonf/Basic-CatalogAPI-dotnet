97028d7733a808c12749fb1f1bee5d98c271e544
Cleberton Francisco <clebertonfgc@gmail.com>
2024-07-04 22:11:41 -0300

    Add Task and async await

--- CatalogAPI/Controllers/ProductsController.cs
+++ CatalogAPI/Controllers/ProductsController.cs
@@ -18,11 +18,11 @@ namespace CatalogAPI.Controllers
 
         [HttpGet]
         [HttpGet("test")]
-        public IActionResult Get()
+        public async Task<IActionResult> GetAsync()
         {
             try
             {
-                var products = _appDbContext.products.Take(10).AsNoTracking().ToList();
+                var products = await _appDbContext.products.Take(10).AsNoTracking().ToListAsync();
                 if (products is null) return NotFound();
 
                 return Ok(products);
@@ -35,27 +35,27 @@ namespace CatalogAPI.Controllers
         }
 
         [HttpGet("{id:int:min(1)}")]
-        public ActionResult<Product> GetById(int id)
+        public async Task<ActionResult<Product>> GetByIdAsync(int id)
         {
-            var product = _appDbContext.products.FirstOrDefault(p => p.ProductId.Equals(id));
+            var product = await _appDbContext.products.FirstOrDefaultAsync(p => p.ProductId.Equals(id));
             if (product is null) return NotFound("Product not found!");
 
             return product;
         }
 
         [HttpPost]
-        public ActionResult Post([FromBody] Product product)
+        public async Task<ActionResult> PostAsync([FromBody] Product product)
         {
             if (product is null) return BadRequest();
 
-            _appDbContext.products.Add(product);
-            _appDbContext.SaveChanges();
+            await _appDbContext.products.AddAsync(product);
+            await _appDbContext.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetById), new { id = product.ProductId}, product);
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = product.ProductId}, product);
         }
 
         [HttpPut("{id:int}")]
-        public ActionResult Put(int id, [FromBody] Product product)
+        public async Task<ActionResult> PutAsync(int id, [FromBody] Product product)
         {
             if (!id.Equals(product.ProductId)) return BadRequest();
 
@@ -65,25 +65,25 @@ namespace CatalogAPI.Controllers
              _appDbContext.SaveChanges();
             */
 
-            var productData = _appDbContext.products.FirstOrDefault(p => p.ProductId.Equals(id));
+            var productData = await _appDbContext.products.FirstOrDefaultAsync(p => p.ProductId.Equals(id));
             if (productData is null) return NotFound();
 
             productData.Name = product.Name;
             productData.Description = product.Description;
 
-            _appDbContext.SaveChanges();
+            await _appDbContext.SaveChangesAsync();
 
             return Ok(product);
         }
 
         [HttpDelete("{id:int}")]
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> DeleteAsync(int id)
         {
-           var product = _appDbContext.products.FirstOrDefault(p => p.ProductId.Equals(id));
+           var product = await _appDbContext.products.FirstOrDefaultAsync(p => p.ProductId.Equals(id));
            if (product is null) return NotFound();
 
            _appDbContext.products.Remove(product);
-           _appDbContext.SaveChanges();
+           await _appDbContext.SaveChangesAsync();
 
             return NoContent();
         }
