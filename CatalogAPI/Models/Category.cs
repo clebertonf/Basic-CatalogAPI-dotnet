14589a863886ade2a67ac2f7e9831e8e09566ed0
Cleberton Francisco <clebertonfgc@gmail.com>
2024-07-06 21:22:15 -0300

    add validation IValidatableObject

--- CatalogAPI/Models/Category.cs
+++ CatalogAPI/Models/Category.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace CatalogAPI.Models;
 
 [Table("categories")]
-public class Category
+public class Category : IValidatableObject
 {
     public Category()
     {
@@ -20,8 +20,16 @@ public class Category
     [StringLength(100)]
     [ValidName]
     public string? Name { get; set; }
-    [Required]
+    //[Required]
     [StringLength(300)]
     public string? UrlImage { get; set; }
     public ICollection<Product>? Products { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (string.IsNullOrWhiteSpace(this.UrlImage))
+        {
+            yield return new ValidationResult("UrlImage cannot be empty.", new[] { nameof(this.UrlImage)});
+        }
+    }
 }
