9f7b91f77734ae9b3b7fb8b2e7c4fde1dc3e0756
Cleberton Francisco <clebertonfgc@gmail.com>
2024-07-13 22:19:10 -0300

    implementation of the generic repository

---
+++ CatalogAPI/Repositories/Generic/Repository.cs
@@ -0,0 +1,47 @@
+﻿using CatalogAPI.Context;
+using System.Linq.Expressions;
+
+namespace CatalogAPI.Repositories.Generic
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext _appDbContext;
+
+        public Repository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _appDbContext.Set<T>().ToList();
+        }
+
+        public T? Get(Expression<Func<T, bool>> predicate)
+        {
+            return _appDbContext.Set<T>().FirstOrDefault(predicate);
+        }
+
+        public T Create(T entity)
+        {
+            _appDbContext.Set<T>().Add(entity);
+            _appDbContext.SaveChanges();
+            return entity;
+        }
+
+        public T Update(T entity)
+        {
+            // _appDbContext.Entry(entity).State = EntityState.Modified;
+            _appDbContext.Set<T>().Update(entity);
+            _appDbContext.SaveChanges();
+            return entity;
+        }
+
+        public T Delete(T entity)
+        {
+            _appDbContext.Set<T>().Remove(entity);
+            _appDbContext.SaveChanges();
+            return entity;
+        }
+    }
+}
